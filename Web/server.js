596551bb2aa872cfeab0d2ee61af42da6f3d197b
chz160 <noahporch@gmail.com>
2014-12-13 14:28:24 -0600

    html server

--- Web/server.js
+++ Web/server.js
@@ -1,6 +1,38 @@
-﻿var http = require('http');
+﻿var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+
 var port = process.env.port || 1337;
+
+var _viewFolder = "views\\";
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    var uri = url.parse(req.url).pathname
+    , filename = path.join(process.cwd(), _viewFolder, uri);
+
+    path.exists(filename, function (exists) {
+        if (!exists) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.write("404 Not Found\n");
+            res.end();
+            return;
+        }
+        
+        if (fs.statSync(filename).isDirectory()) {
+            filename += 'index.html';
+        } 
+        
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.write(err + "\n");
+                res.end();
+                return;
+            }
+            
+            res.writeHead(200);
+            res.write(file, "binary");
+            res.end();
+        });
+    });
 }).listen(port);
