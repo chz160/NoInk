86993ccd80bb7568bad6e6455437b07c4b1d1843
chz160 <noahporch@gmail.com>
2014-12-13 16:21:31 -0600

    Added lout

--- Web/server.js
+++ Web/server.js
@@ -3,17 +3,26 @@ var port = parseInt(process.env.PORT, 10) || 1337;
 var server = new hapi.Server();
 server.connection({ port: port });
 
+server.register({ register: require('lout'), options: { endpoint: '/docs'} }, function (err) {
+    console.log(err);
+});
+
 var api = require("./routes/api");
 server.route(api);
 
 server.route({
     method: 'GET',
     path: '/{path*}',
-    handler: {
-        directory: {
-            path: './views',
-            listing: false,
-            index: true
+    config: {
+        handler: {
+            directory: {
+                path: './views',
+                listing: false,
+                index: true
+            }
+        },
+        plugins: {
+            lout: false
         }
     }
 });
