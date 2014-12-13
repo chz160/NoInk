ebed75eef7e6685784a1644f33cce9e17a7824f8
dlawless <derek@dereklawless.com>
2014-12-13 14:50:01 -0500

    Connecting to Mongo

--- Web/server.js
+++ Web/server.js
@@ -1,5 +1,14 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
+
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var url = 'mongodb://noinkuser:password1@ds061248.mongolab.com:61248/multivision';
+
+MongoClient.connect(url, function(err, db) {
+    console.log("Connected to mongo");
+    db.close();
+});
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
