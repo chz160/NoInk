97d1fc799d589c5e9ce6706f5b5fe62b010f0c6e
chz160 <noahporch@gmail.com>
2014-12-14 13:18:37 -0600

    Authentication

--- Web/services/authenticationService.js
+++ Web/services/authenticationService.js
@@ -1,14 +1,33 @@
-﻿var DataServiceType = require('../mongo-stuff.js');
+﻿var hapi = require("hapi");
+var joi = require("joi");
+
+var DataServiceType = require('../mongo-stuff.js');
 var dataService = new DataServiceType();
 
 exports.register = function (request, reply) {
     reply(true);
 }
 
-exports.login = function (request, reply) {
-    reply(true);
+exports.login = {
+    //validate: {
+    //    payload: {
+    //        email: joi.string().email().required(),
+    //        password: joi.string().required()
+    //    }
+    //},
+    handler: function (request, reply) {
+        reply('Hi your e-mail is "' + request.payload.email + '", that\'s all!');
+    }
 }
 
-exports.logout = function (request, reply) {
-    reply(true);
+exports.register = {
+    //validate: {
+    //    payload: {
+    //        email: joi.string().email().required(),
+    //        password: joi.string().required()
+    //    }
+    //},
+    handler: function (request, reply) {
+        reply('Hi your e-mail is "' + request.payload.email + '", that\'s all!');
+    }
 }
