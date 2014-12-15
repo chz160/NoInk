f634318637ca46454c36e0e7ac0b5bbb4da22e0c
chz160 <noahporch@gmail.com>
2014-12-15 09:39:54 -0600

    Handlebars

--- Web/server.js
+++ Web/server.js
@@ -31,15 +31,16 @@ server.register([
         clientSecret: 'my_twitter_client_secret',
         isSecure: false     // Terrible idea but required if not using HTTPS
     });
-        
+    
+    //Load the API routes
+    var api = require("./routes/api");
+    server.route(api);
+
     // Use the 'twitter' authentication strategy to protect the
     // endpoint handling the incoming authentication credentials.
     // This endpoints usually looks up the third party account in
     // the database and sets some application state (cookie) with
     // the local application account information.
-    var api = require("./routes/api");
-    server.route(api);
-
     server.route([
         {
             method: ['GET', 'POST'], // Must handle both GET and POST
@@ -64,37 +65,11 @@ server.register([
         },
         { method: 'GET', path: '/', handler: function (request, reply) { reply.view('index'); } },
         { method: 'GET', path: '/submissions', handler: function (request, reply) { reply.view('submissions'); } },
-        {
-            method: 'GET',
-            path: '/scripts/{path*}',
-            config: {
-                handler: {
-                    directory: {
-                        path: './node_modules',
-                        listing: false,
-                        index: false
-                    }
-                },
-                plugins: {
-                    lout: false
-                }
-            }
-        }, {
-            method: 'GET',
-            path: '/public/{path*}',
-            config: {
-                handler: {
-                    directory: {
-                        path: './public',
-                        listing: false,
-                        index: true
-                    }
-                },
-                plugins: {
-                    lout: false
-                }
-            }
-        }
+        { method: 'GET', path: '/register', handler: function (request, reply) { reply.view('register'); } },
+        { method: 'GET', path: '/requestSubmission', handler: function (request, reply) { reply.view('requestSubmission'); } },
+        { method: 'GET', path: '/emergencyContact', handler: function (request, reply) { reply.view('emergencyContact'); } },
+        { method: 'GET', path: '/scripts/{path*}', config: { handler: { directory: { path: './node_modules', listing: false, index: false } }, plugins: { lout: false } } }, 
+        { method: 'GET', path: '/public/{path*}', config: { handler: { directory: { path: './public', listing: false, index: true } }, plugins: { lout: false } } }
     ]);
 });
 
