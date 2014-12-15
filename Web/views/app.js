682da48ed441f2683f0716c786c73f7e88b71053
dlawless <derek@dereklawless.com>
2014-12-15 10:22:41 -0500

    Refactoring...

--- Web/views/app.js
+++ Web/views/app.js
@@ -28,61 +28,23 @@ noInkApp.controller('DataCtrl', function ($scope, $http, $filter) {
     
     $scope.sexes = ["Male", "Female"];
     
-    $scope.stateList = [
-        { name: "Alabama", abbr: "AL" },
-        { name: "Alaska", abbr: "AK" },
-        { name: "Arizona", abbr: "AZ" },
-        { name: "Arkansas", abbr: "AR" },
-        { name: "California", abbr: "CA" },
-        { name: "Colorado", abbr: "CO" },
-        { name: "Connecticut", abbr: "CT" },
-        { name: "Delaware", abbr: "DE" },
-        { name: "Florida", abbr: "FL" },
-        { name: "Georgia", abbr: "GA" },
-        { name: "Hawaii", abbr: "HI" },
-        { name: "Idaho", abbr: "ID" },
-        { name: "Illinois", abbr: "IL" },
-        { name: "Indiana", abbr: "IN" },
-        { name: "Iowa", abbr: "IA" },
-        { name: "Kansas", abbr: "KS" },
-        { name: "Kentucky", abbr: "KY" },
-        { name: "Louisiana", abbr: "LA" },
-        { name: "Maine", abbr: "ME" },
-        { name: "Maryland", abbr: "MD" },
-        { name: "Massachusetts", abbr: "MA" },
-        { name: "Michigan", abbr: "MI" },
-        { name: "Minnesota", abbr: "MN" },
-        { name: "Mississippi", abbr: "MS" },
-        { name: "Missouri", abbr: "MO" },
-        { name: "Montana", abbr: "MT" },
-        { name: "Nebraska", abbr: "NE" },
-        { name: "Nevada", abbr: "NV" },
-        { name: "New Hampshire", abbr: "NH" },
-        { name: "New Jersey", abbr: "NJ" },
-        { name: "New Mexico", abbr: "NM" },
-        { name: "New York", abbr: "NY" },
-        { name: "North Carolina", abbr: "NC" },
-        { name: "North Dakota", abbr: "ND" },
-        { name: "Ohio", abbr: "OH" },
-        { name: "Oklahoma", abbr: "OK" },
-        { name: "Oregon", abbr: "OR" },
-        { name: "Pennsylvania", abbr: "PA" },
-        { name: "Rhode Island", abbr: "RI" },
-        { name: "South Carolina", abbr: "SC" },
-        { name: "South Dakota", abbr: "SD" },
-        { name: "Tennessee", abbr: "TN" },
-        { name: "Texas", abbr: "TX" },
-        { name: "Utah", abbr: "UT" },
-        { name: "Vermont", abbr: "VT" },
-        { name: "Virginia", abbr: "VA" },
-        { name: "Washington", abbr: "WA" },
-        { name: "Washington DC", abbr: "DC" },
-        { name: "West Virginia", abbr: "WV" },
-        { name: "Wisconsin", abbr: "WI" },
-        { name: "Wyoming", abbr: "WY" },
-    ];
-
     $scope.emergencyContactTemplate = { name: 'emergencyContact.html', url: 'emergencyContact.html' };
+    
+    $scope.getStates = function () {
+        $http({
+            method: "GET",
+            url: "/api/getStates"
+        }).success(function (data) {
+            $scope.stateList = data;
+            //console.log(data);
+        }).error(function (data, status, header, config) {
+            console.log(data);
+            console.log(status);
+            console.log(header);
+            console.log(config);
+        });
+    };
+
 
     $scope.save = function (newSubmission) {
         $http({
@@ -102,6 +64,8 @@ noInkApp.controller('DataCtrl', function ($scope, $http, $filter) {
     $scope.removeEmergencyContact = function(ec) {
         $scope.person.emergencyContacts.pop(ec);
     }
+
+    $scope.getStates();
 });
 
 
@@ -112,7 +76,7 @@ noInkApp.controller('SubmissionCtrl', function ($scope, $http) {
             url: "/api/getSubmissions"
         }).success(function(data) {
             $scope.submissions = data;
-            console.log(data);
+            //console.log(data);
             $scope.detailView = false;
         }).error(function(data, status, header, config) {
             console.log(data);
