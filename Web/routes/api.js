2a6e1ba72eb9dfd7bddb16c21fefbee0634593e1
chz160 <noahporch@gmail.com>
2014-12-17 00:57:30 -0600

    Cleanup

--- Web/routes/api.js
+++ Web/routes/api.js
@@ -7,17 +7,6 @@ var authenticationService = require(path.join(appDir, '../services/authenticatio
 var somethingService = require(path.join(appDir, '../services/somethingService.js'));
 var mailService = require(path.join(appDir, '../services/mailService.js'));
 
-//module.exports = [
-//    { method: 'POST', path: '/api/login', config: authenticationService.login },
-//    { method: 'GET', path: '/api/logout', config: authenticationService.logout },
-//    { method: 'POST', path: '/api/register', config: authenticationService.register },
-//    { method: "POST", path: "/api/saveForm", config: { handler: somethingService.saveForm, validate: { query: { name: joi.string() } } } },
-//    { method: "GET", path: "/api/getSubmissions", config: { handler: somethingService.getSubmissions, validate: { query: { name: joi.string() } } } },
-//    { method: "GET", path: "/api/getSubmissionDetail", config: { handler: somethingService.getSubmissionDetail } },
-//    { method: "POST", path: "/api/sendMail", config: { handler: mailService.sendMail, validate: { query: { name: joi.string() } } } },
-//    { method: "GET", path: "/api/getStates", config: { handler: somethingService.getStates } }
-//];
-
 module.exports = [
     router.get('/login', authenticationService.login),
     router.get('/logout', authenticationService.logout),
