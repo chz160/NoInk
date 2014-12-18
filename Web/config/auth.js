eeb6863b1aa01fcf5b1660b562acb5027c740e80
chz160 <noahporch@gmail.com>
2014-12-17 23:42:37 -0600

    Linking of external accounts to one user

--- Web/config/auth.js
+++ Web/config/auth.js
@@ -1,17 +1,66 @@
-﻿module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '330754070441547', // your App ID
-        'clientSecret'  : '871d0a922840fb27a97098ca9ee1417c', // your App Secret
-        'callbackURL'   : 'http://localhost:1337/auth/facebook/callback'
-    },
-    'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:1337/auth/twitter/callback'
-    },
-    'googleAuth' : {
-        'clientID'      : '716573943630-5qiccl0hvaek6ai9cb7crqujnjjhecho.apps.googleusercontent.com',
-        'clientSecret'  : 'GqWJpFGFXU81Z46JmvniqfnA',
-        'callbackURL'   : 'http://localhost:1337/auth/google/callback'
+﻿var Confidence = require("confidence");
+
+var store = new Confidence.Store({
+    'provider': {
+        '$filter': 'env',
+        'development': {
+            'facebookAuth' : {
+                'clientID'      : '330754070441547', // your App ID
+                'clientSecret'  : '871d0a922840fb27a97098ca9ee1417c', // your App Secret
+                'callbackURL'   : 'http://localhost:1337/auth/facebook/callback'
+            },
+            'twitterAuth' : {
+                'consumerKey'       : 'X74DWvl8YtNsuzI0E84d45rXT',
+                'consumerSecret'    : 'jKoS00dEsUgmRzWf492aK5ZnQnn18LMWYZ8eFTiTKoxjpFejBX',
+                'callbackURL'       : 'http://localhost:1337/auth/twitter/callback'
+            },
+            'googleAuth' : {
+                'clientID'      : '716573943630-5qiccl0hvaek6ai9cb7crqujnjjhecho.apps.googleusercontent.com',
+                'clientSecret'  : 'GqWJpFGFXU81Z46JmvniqfnA',
+                'callbackURL'   : 'http://localhost:1337/auth/google/callback'
+            }
+        },
+        '$default': {
+            'facebookAuth' : {
+                'clientID'      : '330752823775005', // your App ID
+                'clientSecret'  : 'd357ece0c4a1c16f9e72110172c79277', // your App Secret
+                'callbackURL'   : 'http://ec2-54-68-202-243.us-west-2.compute.amazonaws.com/auth/facebook/callback'
+            },
+            'twitterAuth' : {
+                'consumerKey'       : 'X74DWvl8YtNsuzI0E84d45rXT',
+                'consumerSecret'    : 'jKoS00dEsUgmRzWf492aK5ZnQnn18LMWYZ8eFTiTKoxjpFejBX',
+                'callbackURL'       : 'http://ec2-54-68-202-243.us-west-2.compute.amazonaws.com/auth/twitter/callback'
+            },
+            'googleAuth' : {
+                'clientID'      : '716573943630-ta92nqaesirpgtloour4qklb2t848qfr.apps.googleusercontent.com',
+                'clientSecret'  : 'smSH5Yi5t1j28Fj5TdH3fyvl',
+                'callbackURL'   : 'http://ec2-54-68-202-243.us-west-2.compute.amazonaws.com/auth/google/callback'
+            }
+        }
     }
+});
+
+var criteria = {
+    env: process.env.ENVIRONMENT
 };
+
+exports.get = function (key) {
+    return store.get(key, criteria);
+};
+//module.exports = {
+//    'facebookAuth' : {
+//        'clientID'      : '330754070441547', // your App ID
+//        'clientSecret'  : '871d0a922840fb27a97098ca9ee1417c', // your App Secret
+//        'callbackURL'   : 'http://localhost:1337/auth/facebook/callback'
+//    },
+//    'twitterAuth' : {
+//        'consumerKey'       : 'X74DWvl8YtNsuzI0E84d45rXT',
+//        'consumerSecret'    : 'jKoS00dEsUgmRzWf492aK5ZnQnn18LMWYZ8eFTiTKoxjpFejBX',
+//        'callbackURL'       : 'http://localhost:1337/auth/twitter/callback'
+//    },
+//    'googleAuth' : {
+//        'clientID'      : '716573943630-5qiccl0hvaek6ai9cb7crqujnjjhecho.apps.googleusercontent.com',
+//        'clientSecret'  : 'GqWJpFGFXU81Z46JmvniqfnA',
+//        'callbackURL'   : 'http://localhost:1337/auth/google/callback'
+//    }
+//};
--- Web/routes/views.js
+++ Web/routes/views.js
@@ -3,24 +3,37 @@
     //var router = express.Router();
     app.get('/', function (req, res) { res.render('index'); });
     
-    app.get('/login', function (req, res) { res.render('login', { message: req.flash('loginMessage') }); });
-    app.post('/login', passport.authenticate('local-login', { successRedirect : '/profile', failureRedirect : '/login', failureFlash : true }));
-    
     app.get('/signup', function (req, res) { res.render('signup', { message: req.flash('signupMessage') }); });
     app.post('/signup', passport.authenticate('local-signup', { successRedirect : '/profile', failureRedirect : '/signup', failureFlash : true }));
-    
-    app.get('/profile', isLoggedIn, function (req, res) { res.render('profile', { user : req.user }); });
+    app.get('/login', function (req, res) { res.render('login', { message: req.flash('loginMessage') }); });
+    app.post('/login', passport.authenticate('local-login', { successRedirect : '/profile', failureRedirect : '/login', failureFlash : true }));
     app.get('/logout', function (req, res) { req.logout(); res.redirect('/'); });
+    app.get('/profile', isLoggedIn, function (req, res) { res.render('profile', { user : req.user }); });
+    app.get('/connect/local', function (req, res) { res.render('connect-local', { message: req.flash('loginMessage') }); });
+    app.post('/connect/local', passport.authenticate('local-signup', { successRedirect : '/profile', failureRedirect : '/connect/local', failureFlash : true }));
+    app.get('/unlink/local', function (req, res) { var user = req.user; user.local.email = undefined; user.local.password = undefined; user.save(function (err) { res.redirect('/profile'); }); });
     
+    // facebook -------------------------------
     app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
     app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect : '/profile', failureRedirect : '/' }));
+    app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+    app.get('/connect/facebook/callback', passport.authorize('facebook', { successRedirect : '/profile', failureRedirect : '/' }));
+    app.get('/unlink/facebook', function (req, res) { var user = req.user; user.facebook.token = undefined; user.save(function (err) { res.redirect('/profile'); }); });
     
+    // twitter --------------------------------
     app.get('/auth/twitter', passport.authenticate('twitter'));
     app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect : '/profile', failureRedirect : '/' }));
-
+    app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+    app.get('/connect/twitter/callback', passport.authorize('twitter', { successRedirect : '/profile', failureRedirect : '/' }));
+    app.get('/unlink/twitter', function (req, res) { var user = req.user; user.twitter.token = undefined; user.save(function (err) { res.redirect('/profile'); }); });
+    
+    // google ---------------------------------
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
     app.get('/auth/google/callback', passport.authenticate('google', { successRedirect : '/profile', failureRedirect : '/' }));
-
+    app.get('/connect/google', passport.authorize('google', { scope : ['profile', 'email'] }));
+    app.get('/connect/google/callback', passport.authorize('google', { successRedirect : '/profile', failureRedirect : '/' }));
+    app.get('/unlink/google', function (req, res) { var user = req.user; user.google.token = undefined; user.save(function (err) { res.redirect('/profile'); }); });
+    
     app.get('/form', function (req, res) { res.render('form'); });
     app.get('/submissions', isLoggedIn, function (req, res) { res.render('submissions'); });
     app.get('/requestSubmission', isLoggedIn, function (req, res) { res.render('requestSubmission'); });
