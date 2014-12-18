91327f9a55e7e87119819c14e0165a1adaac645f
chz160 <noahporch@gmail.com>
2014-12-17 22:22:28 -0600

    Authentication - Twitter

---
+++ Web/config/auth.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+    'facebookAuth' : {
+        'clientID'      : '330754070441547', // your App ID
+        'clientSecret'  : '871d0a922840fb27a97098ca9ee1417c', // your App Secret
+        'callbackURL'   : 'http://localhost:1337/auth/facebook/callback'
+    },
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:1337/auth/twitter/callback'
+    },
+    'googleAuth' : {
+        'clientID'      : '716573943630-5qiccl0hvaek6ai9cb7crqujnjjhecho.apps.googleusercontent.com',
+        'clientSecret'  : 'GqWJpFGFXU81Z46JmvniqfnA',
+        'callbackURL'   : 'http://localhost:1337/auth/google/callback'
+    }
+};
--- Web/routes/views.js
+++ Web/routes/views.js
@@ -15,6 +15,9 @@
     app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
     app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect : '/profile', failureRedirect : '/' }));
     
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+    app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect : '/profile', failureRedirect : '/' }));
+
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
     app.get('/auth/google/callback', passport.authenticate('google', { successRedirect : '/profile', failureRedirect : '/' }));
 
