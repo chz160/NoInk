e89f64ed44a9adcb5f41bfadfe41a30e2e5c2672
dlawless <derek@dereklawless.com>
2014-12-20 10:38:23 -0500

    Switched to mongoskin

--- Web/services/dataService.js
+++ Web/services/dataService.js
@@ -1,45 +1,82 @@
-﻿var MongoClient = require('mongodb').MongoClient, 
-    assert = require('assert');
-    ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://noinkuser:password1@ds061248.mongolab.com:61248/multivision';
+﻿var url = 'mongodb://noinkuser:password1@ds061248.mongolab.com:61248/multivision';
+var mongoskin = require('mongoskin');
+var db = mongoskin.db(url);
 
-exports.insertDocument = function(collectionName, doc) {
-    MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            console.log("Connected to mongo...");
-            var collection = db.collection(collectionName);
-            collection.insert(doc, { w: 1 }, function(err, result) {
-                console.log(err);
-                if (err) throw err;
-                db.close();
-            });
+
+exports.insertDocument = function (collectionName, doc) {
+    db.collection(collectionName).insert(doc, {w:1}, function(err, results) {
+        if (err) {
+            console.log(err);
+            throw err;
         }
-    );
+    });
 };
 
 exports.getCollectionItems = function(collectionName, fn) {
-    MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            console.log("Connected to mongo...");
-            var collection = db.collection(collectionName);
-            collection.find().toArray(function(err, docs) {
-                fn(docs);
-            });
-        }
-    );
+    db.collection(collectionName).find({}).toArray(function(err, results) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }        
+        fn(results);
+    });
 };
 
 exports.getCollectionItem = function(collectionName, id, fn) {
-    MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            console.log("Connected to mongo...");
-            var collection = db.collection(collectionName);
-            collection.findOne({ _id: new ObjectId(id) }, function(err, doc) {
-                fn(doc);
-            });
+    db.collection(collectionName).findById(id, function(err, result) {
+        if (err) {
+            console.log(err);
+            throw err;
         }
-    );
+        fn(result);
+    });
 };
+
+
+//var MongoClient = require('mongodb').MongoClient, 
+//    assert = require('assert');
+//ObjectId = require('mongodb').ObjectID;
+
+//exports.insertDocument = function (collectionName, doc) {
+//    MongoClient.connect(url, function (err, db) {
+//        if (err) throw err;
+//        console.log("Connected to mongo...");
+//        var collection = db.collection(collectionName);
+//        collection.insert(doc, { w: 1 }, function (err, result) {
+//            console.log(err);
+//            if (err) throw err;
+//            db.close();
+//        });
+//    }
+//    );
+//};
+
+//exports.getCollectionItems = function (collectionName, fn) {
+//    MongoClient.connect(url, function (err, db) {
+//        if (err) throw err;
+//        console.log("Connected to mongo...");
+//        var collection = db.collection(collectionName);
+//        collection.find().toArray(function (err, docs) {
+//            fn(docs);
+//        });
+//    }
+//    );
+//};
+
+//exports.getCollectionItem = function (collectionName, id, fn) {
+//    MongoClient.connect(url, function (err, db) {
+//        if (err) throw err;
+//        console.log("Connected to mongo...");
+//        var collection = db.collection(collectionName);
+//        collection.findOne({ _id: new ObjectId(id) }, function (err, doc) {
+//            fn(doc);
+//        });
+//    }
+//    );
+//};
+
+
+
 
 exports.insertFormSubmission = function (doc) {
     this.insertDocument("formsubmissions", doc);
@@ -56,3 +93,4 @@ exports.getSubmissions = function (fn) {
         fn(docs);
     });
 };
+
