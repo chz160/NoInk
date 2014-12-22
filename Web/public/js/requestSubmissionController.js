8032c5cb9daf7b62e60133b104b459030320abf4
dlawless <derek@dereklawless.com>
2014-12-21 21:38:47 -0500

    Upgrade to the Request Submission email functionality.

--- Web/public/js/requestSubmissionController.js
+++ Web/public/js/requestSubmissionController.js
@@ -1,10 +1,17 @@
 ﻿noInkApp.controller('RequestSubmissionController', function ($scope, $http) {
-    $scope.requestSubmission = function () {
-        var email = $scope.email;
+    $scope.requestSubmission = function (req) {
         $http({
             method: "POST",
             url: "/api/sendMail",
-            data: $scope
+            data: req
+        }).success(function(result) {
+            $scope.req = {};
+            $scope.requestSubmissionForm.$setPristine();
+            $scope.sendError = false;
+            $scope.sendSuccess = true;
+        }).error(function(err) {
+            console.log('Failed to send message');
+            $scope.sendError = true;
         });
     };
 });
--- Web/services/mailService.js
+++ Web/services/mailService.js
@@ -1,22 +1,35 @@
 ﻿var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
-var options = {
+var transportOptions = {
     auth: {
         api_user: 'dlawless',
         api_key: 'password1'
     }
 };
-var transporter = nodemailer.createTransport(sgTransport(options));
+var transporter = nodemailer.createTransport(sgTransport(transportOptions));
 
-exports.sendMail = function(reg, res) {
-    transporter.sendMail({
-        from: 'derekhlawless@gmail.com',
-        to: 'derekhlawless@gmail.com',
-        subject: '42',
-        text: 'Is the answer to life...'
-    }, function(err, info) {
-        console.log(err);
+exports.sendMail = function(req, res) {
+    var requestData = req.body;
+    var from = 'derekhlawless@gmail.com';
+    var subject = 'Employment Application Request';
+    var url = 'http://ec2-54-68-202-243.us-west-2.compute.amazonaws.com/form';
+    var messageTemplate = '<p>' + requestData.firstName + ' ' + requestData.lastName + ', your employer needs some additional information from you. Please visit <a href=\'' + url + '\'>Employment Application Form</a> and provide your information.<p>The NoInk Team</p>';
+    console.log(messageTemplate);
+
+    var mailOptions = {
+        from: from,
+        to: requestData.email,
+        subject: subject,
+        html: messageTemplate
+    };
+
+    transporter.sendMail(mailOptions, function(err, info) {
+        if (err) {
+            console.log(err);
+            res.send({ msg: 'failure' });
+        } else
+            res.send({ msg: 'success' });
     });
     return true;
-}
+};
 
